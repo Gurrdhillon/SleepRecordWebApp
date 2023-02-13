14d0508d69010466cf653f74ca9be6309487f83f
Gursevak Singh <78093727+Gurrdhillon@users.noreply.github.com>
2023-02-12 18:42:13 -0800

    sleep record page functionality complete

--- FrontEnd/src/components/sleepForm.js
+++ FrontEnd/src/components/sleepForm.js
@@ -10,11 +10,6 @@ import Image from '/Users/gursevak/Projects/SleepRecordWebApp/FrontEnd/src/image
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 
-/**
- * convert enter date to select date later on, 
- * React itself has select date feature, so used can either enter it
- * or select from given calender
- */
 
  const divStyle = {
   color: 'black',
@@ -30,23 +25,37 @@ import Box from "@mui/material/Box";
 export default function SleepForm() {
 
   const [submitting, setSubmitting] = useState(false)
+  const [date, setDate] =  useState();
+  const [hours, setHours] = useState();
 
   const handleSubmit = event => {
-    event.preventDefault();
+
+    event.preventDefault();   //It prevent the default submit behaviour, which is that 
+                              //it will clear the page once the user hit the submit button
+
+    // So whats happening here, date and hours getting their data updated as states
+    // as onChnage function there make changes the value in state, as these are variables
+    //it also get changed here
+
+     const record = {date, hours};    
+     
+     fetch('http://localhost:3001/api/insert',{
+      method: 'POST',
+      headers : {"Content-Type": "application/json"},
+      body: JSON.stringify(record)
+     }).then (() => {
+        setSubmitting(false)
+      console.log("new record added, hip hip hurray")
+     })
+  
 
     setSubmitting(true)
 
-    setTimeout(()=> {
-      alert('delayed on purpose')
-      setSubmitting(false)
-    },2000)
-
-
+    // setTimeout(()=> {
+    //   alert('delayed on purpose')
+    //   setSubmitting(false)
+    // },2000)
   }
-
-  useEffect(() => {
-    alert('Page Refreshd again.')
-  }); 
 
   /*
   For spinning circlle for the setTimeout, 2 seconds, later used to wait for 
@@ -63,40 +72,37 @@ export default function SleepForm() {
     <div style={divStyle}>
       {/* <div style={{  justifyContent: "center", alignItems: "center", height: "100vh" , width: '100vw'}}> */}
       <div style = {{padding: "50px"}}>
-    <Form onSubmit={handleSubmit} >
-      <Form.Group as={Row} className="mb-1" controlId="formBasicDate">
-        <Form.Label column sm={3} >Date</Form.Label> 
-        <Col sm={5}>  
-        <Form.Control type="date" placeholder="Date" />
-        </Col>
-      </Form.Group>
+        <Form onSubmit={handleSubmit} >
+          <Form.Group as={Row} className="mb-1" controlId="formBasicDate">
+            <Form.Label column sm={3} >Date</Form.Label> 
+            <Col sm={5}>  
+            <Form.Control 
+            type="date" 
+            placeholder="Date" 
+            value = {date}
+            onChange = {(e) => setDate(e.target.value)}
+            />
+            </Col>
+          </Form.Group>
 
-      {/* <Form.Group as={Row} className="mb-1" controlId="formBasicTime">
-        <Form.Label column sm={3} >Sleep In</Form.Label>
-        <Col sm={5}>
-        <Form.Control type="time" placeholder="Sleep In" />
-        </Col>
-      </Form.Group>
+          <Form.Group as={Row} className="mb-3" controlId="formBasicHour">
+            <Form.Label column sm={3} >Hours</Form.Label>
+            <Col sm={5}>
+            <Form.Control 
+            type="hour"
+            placeholder="Sleeping Hours" 
+            value = {hours}
+            onChange = {(e) => setHours(e.target.value)}
+            />
+            </Col>
+          </Form.Group>
 
-      <Form.Group as={Row} className="mb-1" controlId="formBasicTime">
-        <Form.Label column sm={3} >Sleep Out</Form.Label>
-        <Col sm={5}>
-        <Form.Control type="time" placeholder="Sleep Out" />
-        </Col>
-      </Form.Group> */}
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
 
-      <Form.Group as={Row} className="mb-3" controlId="formBasicHour">
-        <Form.Label column sm={3}  >Hours</Form.Label>
-        <Col sm={5}>
-        <Form.Control type="hour" placeholder="Sleeping Hours" />
-        </Col>
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    </div>
+      </div>
     </div>
       );
   }
